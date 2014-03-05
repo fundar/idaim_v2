a27aa30679c9167e3c55f9c64f335bae30864acb
Jaime Rodas <jaime@surrealista.mx>
2014-03-05 16:30:48 -0600

    Acomodamos en contenedores correctos y empezamos con media queries

--- public/index.php
+++ public/index.php
@@ -77,25 +77,18 @@
 				</div>
 			</section>
 
-			<section id="indice-nacional">
+			<section id="indice-nacional" class="sec-b">
 				<div class="container">
-					<div id="graph-indices-nacional">
-						
-					</div>
+					<div id="graph-indices-nacional"></div>
 				</div>
 			</section>
 
-			<section id="mapa">
+			<section id="mapa" class="sec-a">
 				<div class="container">
 					<?= file_get_contents('./img/mapa.svg') ;?>
 				</div>
 			</section>
 
-			<section class="sec-a">
-				<div class="container">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</div>
-			</section>
 			<footer>
 				<div class="container">
 					<div id="logo-fundar"></div>
