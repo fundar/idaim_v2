e8b44ac1a8f1c60f0b3188c7555e106c5e8fbaf7
Jaime Rodas <jaime@surrealista.mx>
2014-03-05 12:26:14 -0600

    Empezamos la portada y agregamos attache

--- public/index.php
+++ public/index.php
@@ -13,70 +13,84 @@
 			<!--[if lt IE 8]>
 					<p class="browsehappy">Estás usando un navegador <strong>anticuado</strong>. Por favor <a href="http://browsehappy.com/?locale=es">actualiza tu navegador</a> para mejorar tu experiencia.</p>
 			<![endif]-->
+			<div id="container">
+				<div id="geolocated">
 
-			<div id="geolocated">
-				
+				</div>
+
+				<header>
+						<div id="logo"></div>
+						<nav>
+								<ul>
+										<li><a href="">Quiénes somos</a></li>
+										<li><a href="">Acerca de</a></li>
+										<li><a href="">Contacto</a></li>
+								</ul>
+
+								<ul>
+										<li><a href="">email</a></li>
+										<li><a href="">twitter</a></li>
+										<li><a href="">facebook</a></li>
+								</ul>
+						</nav>
+				</header>
+
+				<section id="hero">
+					<div id="hero-text">
+						<span class="hero-text l1">El IDAIM</span>
+						<span class="hero-text l2">mide las</span>
+						<span class="hero-text l3">Leyes de Transparencia</span>
+						<span class="hero-text l4">y acceso a la</span>
+						<span class="hero-text l5">Información en México</span>
+						<span class="hero-text l6">con relación a las</span>
+						<span class="hero-text l7">mejores prácticas</span>
+						<span class="hero-text l8">Nacionales e Internacionales</span>
+					</div>
+				</section>
+
+				<section>
+						<div>Estás visitándonos desde</div>
+						<h1>Aguascalientes</h1>
+
+				</section>
+
+				<section id="nacional">
+					<h2 class="pc" id="total-nacional"></h2>
+					<div class="graph-total">
+
+					</div>
+					<div id="total-ultimo">
+						<h2 class="pc"></h2>
+						<h3></h3>
+					</div>
+					<div id="total-primero">
+						<h2 class="pc"></h2>
+						<h3></h3>
+					</div>
+				</section>
+
+				<section>
+
+				</section>
+
+				<footer>
+						<div id="logo-fundar"></div>
+						<div id="copyright">&copy; 2014 Fundar. Todos los derechos reservados</div>
+						<nav>
+								<ul>
+										<li><a href="">Quiénes somos</a></li>
+										<li><a href="">Acerca de</a></li>
+										<li><a href="">Contacto</a></li>
+										<li><a href="">IDAIM 2010</a></li>
+								</ul>
+
+								<ul>
+										<li><a href="">Aviso de Privacidad</a></li>
+										<li><a href="">Ayuda</a></li>
+								</ul>
+						</nav>
+				</footer>
 			</div>
-
-			<header>
-					<div id="logo"></div>
-					<nav>
-							<ul>
-									<li><a href="">Quiénes somos</a></li>
-									<li><a href="">Acerca de</a></li>
-									<li><a href="">Contacto</a></li>
-							</ul>
-
-							<ul>
-									<li><a href="">email</a></li>
-									<li><a href="">twitter</a></li>
-									<li><a href="">facebook</a></li>
-							</ul>
-					</nav>
-
-			</header>
-
-			<section>
-					<div>Estás visitándonos desde</div>
-					<h1>Aguascalientes</h1>
-
-			</section>
-
-			<section id="nacional">
-				<h2 class="pc" id="total-nacional"></h2>
-				<div class="graph-total">
-					
-				</div>
-				<div id="total-ultimo">
-					<h2 class="pc"></h2>
-					<h3></h3>
-				</div>
-				<div id="total-primero">
-					<h2 class="pc"></h2>
-					<h3></h3>
-				</div>
-			</section>
-
-			<section>
-
-			</section>
-
-			<footer>
-					<div id="copyright">&copy; 2014 Fundar. Todos los derechos reservados</div>
-					<nav>
-							<ul>
-									<li><a href="">Quiénes somos</a></li>
-									<li><a href="">Acerca de</a></li>
-									<li><a href="">Contacto</a></li>
-									<li><a href="">IDAIM 2010</a></li>
-							</ul>
-
-							<ul>
-									<li><a href="">Aviso de Privacidad</a></li>
-									<li><a href="">Ayuda</a></li>
-							</ul>
-					</nav>
-			</footer>
 			<script src="/js/main.js"></script>
 		</body>
 </html>
