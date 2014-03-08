e447ccc951dcf391666b71b6c1f025f969830743
Jaime Rodas <jaime@surrealista.mx>
2014-03-08 08:22:12 -0600

    Hacemos resizeable la gráfica grande

--- public/js/graficaTotal.js
+++ public/js/graficaTotal.js
@@ -1,66 +1,89 @@
 
-var w = 1024,
-    h = 240,
-    x = d3.scale.linear().range([0, w]),
-    y = d3.scale.linear().range([0, h]);
-
-var vis = d3.select("#graph-total").append("div")
-    .attr("class", "chart")
-    .style("width", w + "px")
-    .style("height", h + "px")
-  .append("svg:svg")
-    .attr("width", w)
-    .attr("height", h);
-
-var partition = d3.layout.partition()
-    .value(function(d) { return d.size; })
-    .sort(null);
-
-d3.json("data/estructura.json", function(root) {
-  var rect = vis.selectAll("rect")
-    .data(partition.nodes(root))
-    .enter().append("svg:rect")
-    .attr("x", function(d) { return x(d.x); })
-    .attr("y", function(d) { return y(d.y); })
-    .attr("width", function(d) { return x(d.dx); })
-    .attr("height", function(d) { return y(d.dy); })
-    .attr("class", function(d){return d.class})
-    .attr("id", function(d){return d.id})
-    .attr("fill", color)
-    .on("click", click);
-
-  $('#idaim').addClass('activo');
-
-  function color(d) {
-    var col, set;
-    // var val = d.calificacion;
-    var val = Math.round(Math.random() * 100);
-    var colores = [['73161A', '8B1B1F', 'A31F25', 'BB242A', 'D3282F'], ['E87321', 'ED861E', 'F3981A', 'F8AB17', 'FDBD13'], ['8AC65A', '81BD47', '78B434', '6FAA20', '66A10D']];
-
-    if (val <= 59) {
-      set = colores[0];
-    } else if (val < 80 && val > 59) {
-      set = colores[1];
-    } else {
-      set = colores[2];
-    }
-    col = '#' + set[Math.round((set.length - 1) * val / 100)];
-    return col;
-  };
-
-  function click(d) {
-    if (!d.children) {
-      x.domain([d.parent.x, d.parent.x + d.parent.dx]);
-    } else {
-      x.domain([d.x, d.x + d.dx]);
-    }
-
-    rect.classed('activo', false)
-      .transition()
-      .duration(750)
-      .attr("x", function(d) { return x(d.x); })
-      .attr("width", function(d) { return x(d.x + d.dx) - x(d.x); })
-    
-    $(this).addClass('activo');
-  }
+$(document).ready(function(){
+  dibuja();
 });
+
+function dibuja() {
+  var container = $('#graph-total').empty();
+
+  var w = container.outerWidth(),
+      h = 240,
+      x = d3.scale.linear().range([0, w]),
+      y = d3.scale.linear().range([0, h]);
+
+  var vis = d3.select("#graph-total").append("div")
+      .attr("class", "chart")
+      .style("width", w + "px")
+      .style("height", h + "px")
+    .append("svg:svg")
+      .attr("width", w)
+      .attr("height", h);
+
+  var partition = d3.layout.partition()
+      .value(function(d) { return d.size; })
+      .sort(null);
+
+  d3.json("data/estructura.json", function(root) {
+    var rect = vis.selectAll("rect")
+      .data(partition.nodes(root))
+      .enter().append("svg:rect")
+      .attr("x", function(d) { return x(d.x); })
+      .attr("y", function(d) { return y(d.y); })
+      .attr("width", function(d) { return x(d.dx); })
+      .attr("height", function(d) { return y(d.dy); })
+      .attr("class", function(d){return d.class})
+      .attr("id", function(d){return d.id})
+      .attr("fill", color)
+      .on("click", click);
+
+    $('#idaim').addClass('activo');
+
+    function click(d) {
+      if (!d.children) {
+        x.domain([d.parent.x, d.parent.x + d.parent.dx]);
+      } else {
+        x.domain([d.x, d.x + d.dx]);
+      }
+
+      rect.classed('activo', false)
+        .transition()
+        .duration(750)
+        .attr("x", function(d) { return x(d.x); })
+        .attr("width", function(d) { return x(d.x + d.dx) - x(d.x); })
+      
+      $(this).addClass('activo');
+    }
+  });
+
+}
+
+function color(d) {
+  var col, set;
+  // var val = d.calificacion;
+  var val = Math.round(Math.random() * 100);
+  var colores = [['73161A', '8B1B1F', 'A31F25', 'BB242A', 'D3282F'], ['E87321', 'ED861E', 'F3981A', 'F8AB17', 'FDBD13'], ['8AC65A', '81BD47', '78B434', '6FAA20', '66A10D']];
+
+  if (val <= 59) {
+    set = colores[0];
+  } else if (val < 80 && val > 59) {
+    set = colores[1];
+  } else {
+    set = colores[2];
+  }
+  col = '#' + set[Math.round((set.length - 1) * val / 100)];
+  return col;
+};
+
+var debounce = function(fn, timeout) {
+  var timeoutID = -1;
+  return function() {
+    if (timeoutID > -1) {
+      window.clearTimeout(timeoutID);
+    }
+    timeoutID = window.setTimeout(fn, timeout);
+  }
+};
+
+var debounce_dibuja = debounce(dibuja, 125);
+
+$(window).resize(debounce_dibuja);
