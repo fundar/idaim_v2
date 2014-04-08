34724381f7d4eef2e6253385d360c3887e68ca8c
Jaime Rodas <jaime@surrealista.mx>
2014-04-08 18:07:01 -0500

    Ya se actualizan los datos del estado

--- assets/js/estado.js
+++ assets/js/estado.js
@@ -86,12 +86,21 @@ $(function(){
 			.attr('fill', function(d) {return Color(d.valor); })
 			.attr('d', polygonPath)
 			.on('click', function(d) {
-				selectors = '.indicador, .criterio';
 				if (clase == 'eje') {
 					selectors = '.eje, .indicador, .criterio';
+					descripcion = IDAIM.get('ejes')[d.id];
+					nombre = IDAIM.get('nombres').eje[d.id];
+				} else {
+					selectors = '.indicador, .criterio';
+					descripcion = IDAIM.get('indicadores')[d.id];
+					nombre = IDAIM.get('nombres').indicador[d.id];
 				}
 				d3.selectAll(selectors).classed('activo', 0);
 				d3.select(this).classed('activo', 1);
+
+				$('#calificacion').text(d.valor+'%');
+				$('#nombre').text(nombre)
+				$('#descripcion').text(descripcion);
 			});
 		};
 
@@ -112,6 +121,9 @@ $(function(){
 			.on('click', function(d) {
 				d3.selectAll('.criterio').classed('activo', 0);
 				d3.select(this).classed('activo', 1);
+				$('#calificacion').text(d.valor+'%');
+				$('#nombre').text(IDAIM.get('nombres').criterio[d.id])
+				$('#descripcion').text(' ');
 			});
 
 		var dataIndicadores = [];
@@ -138,6 +150,7 @@ $(function(){
 		var ejes = generaPoligonos('eje', dataEjes);
 
 		var resize = function() {
+			// Calculamos tamaños nuevos
 			wC = $(container).width(),
 			w = wC - (m.r + m.l),
 			h = (w/4),
@@ -152,16 +165,17 @@ $(function(){
 
 			hC = h + (m.t + m.b);
 
-			g.attr("width", wC).attr("height", hC);
+			// Resizeamos
+			g
+				.attr("width", wC)
+				.attr("height", hC);
 			criterios
 				.attr('width', s)
 				.attr('height', s)
 				.attr('x', function(d) { return xSquare(d) * s + m.l;})
 				.attr('y', function(d) { return ySquare(d) * s + m.t;})
-
 			indicadores
 				.attr('d', polygonPath);
-
 			ejes
 				.attr('d', polygonPath);
 
--- public/estado.php
+++ public/estado.php
@@ -39,9 +39,18 @@
 			</header>
 
 
-			<div class="container">
+			<article class="container estado">
+				<h1>Estado de México</h1>
 				<div id="graficaEstado"></div>
-			</div>
+				<div id="calificacion">58%</div>
+				<div id="nombre">Nombre del Eje</div>
+				<div id="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+			</article>
 			
 
 			<footer>
