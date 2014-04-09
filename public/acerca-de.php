6bf0f3c256456adb874342b3b4959be7700c5880
Jaime Rodas <jaime@surrealista.mx>
2014-04-09 14:28:08 -0500

    Agregamos acerca de

---
+++ public/acerca-de.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+		<head>
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<title>IDAIM - Índice del Derecho de Acceso a la Información en México</title>
+				<meta name="description" content="">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<link rel="stylesheet" href="/css/acerca-de.css">
+				<style type="text/css">
+
+    </style>
+		</head>
+		<body>
+			<!--[if lt IE 8]>
+					<p class="browsehappy">Estás usando un navegador <strong>anticuado</strong>. Por favor <a href="http://browsehappy.com/?locale=es">actualiza tu navegador</a> para mejorar tu experiencia.</p>
+			<![endif]-->
+			<script>
+			window._edo = '<?= $edo ?>';
+			</script>
+
+			<header>
+				<div class="container">
+					<a href="/" id="logo">IDAIM</a>
+					<nav id="nav-principal">
+							<ul class="nav-paginas">
+									<li><a href="/quienes-somos">Quiénes somos</a></li>
+									<li><a href="/acerca-de" class="activo">Acerca de</a></li>
+									<li><a href="/contacto">Contacto</a></li>
+							</ul>
+
+							<ul class="nav-social">
+									<li><a class="ss-icon ss-social-regular" href="">email</a></li>
+									<li><a class="ss-icon ss-social-regular" href="">tumblr</a></li>
+									<li><a class="ss-icon ss-social-regular" href="">facebook</a></li>
+							</ul>
+					</nav>
+				</div>
+			</header>
+
+			<div class="acerca-de cf">
+				<article class="container">
+					<h1>Acerca de</h1>
+
+					<p>En esta sección podrás encontrar diferentes documentos derivados de la evaluación de las leyes de transparencia que permiten entender mejor el ejercicio.</p>
+					<ul>
+						<li>
+							<img src="/img/ad-db.jpg" alt="thumb" />
+							<h2>Base de datos</h2>
+							<p>Es un documento en Excel que contiene las calificaciones de los 196 criterios para cada ley evaluada. Sobre este documento se hizo el cálculo del IDAIM.<br />
+							<a href="">Base de datos</a></p>
+						</li>
+						<li>
+							<img src="/img/ad-ft.jpg" alt="thumb" />
+							<h2>Fichas de evaluación por ley de transparencia</h2>
+							<p>Son 33 fichas, una para cada ley de transparencia en México. Cada ficha contiene la calificación de los 196 criterios y el artículo que contiene el criterio evaluado o en caso de tener cero, el que estipula lo contrario. Ocasionalmente no se establece ningún artículo, esto sucede cuando no está contenido en ningún sitio de la ley.<br />
+							<a href="">Fichas por ley de transparencia (zip 30M)</a></p>
+						</li>
+						<li>
+							<img src="/img/ad-fi.jpg" alt="thumb" />
+							<h2>Guía de interpretación</h2>
+							<p>Son 33 fichas, una para cada ley de transparencia en México. Cada ficha contiene la calificación de los 196 criterios y el artículo que contiene el criterio evaluado o en caso de tener cero, el que estipula lo contrario. Ocasionalmente no se establece ningún artículo, esto sucede cuando no está contenido en ningún sitio de la ley.<br />
+							<a href="">Guía de interpretación</a></p>
+							
+						</li>
+						<li>
+							<img src="/img/ad-cu.jpg" alt="thumb" />
+							<h2>Cuadernillo de resultados</h2>
+							<p>En este documento se exponen los resultados del índice, así como su interpretación a la luz del debate actual. El análisis de los resultados presentados es una de muchas formas de hacerlo, para ello está disponible la base de datos de donde pueden obtener la información necesaria para hacer los cruces de información de interés de cada persona. En este cuadernillo se presentan los resultados por indicador, en donde buscamos, sobre todo, dar un panorama general del estado de<br />
+							<a href="">Cuadernillo de resultados</a></p>
+						</li>
+					</ul>
+				</article>
+			</div>
+
+			<footer>
+				<div class="container">
+					<a href="http://fundar.org.mx" id="logo-fundar"></a>
+
+					<nav id="nav-footer">
+							<ul class="footer-paginas">
+									<li><a href="/quienes-somos">Quiénes somos</a></li>
+									<li><a href="/acerca-de" class="activo">Acerca de</a></li>
+									<li><a href="/contacto">Contacto</a></li>
+									<li><a href="http://www.checatuley.org">IDAIM 2010</a></li>
+							</ul>
+
+							<ul class="footer-otros">
+									<li><a href="">Aviso de Privacidad</a></li>
+									<li><a href="">Ayuda</a></li>
+							</ul>
+					</nav>
+
+					<div id="copyright">&copy; 2014 Fundar. Todos los derechos reservados</div>
+				</div>
+			</footer>
+		</body>
+</html>
