4f96c591146a8487762f5b0b1e95a03a50860e75
rob <rob@partidosurrealista.com>
2014-04-28 17:49:37 -0500

    agregando descargables

--- public/documentos.php
+++ public/documentos.php
@@ -24,26 +24,25 @@
 							<img src="/img/ad-db.jpg" alt="thumb" />
 							<h2>Base de datos</h2>
 							<p>Es un documento en Excel que contiene las calificaciones de los 196 criterios para cada ley evaluada. Sobre este documento se hizo el cálculo del IDAIM.<br />
-							<a href="/data/db.xls">Base de datos</a></p>
+							<a href="/data/db.xlsx">Base de datos</a></p>
 						</li>
 						<li>
 							<img src="/img/ad-ft.jpg" alt="thumb" />
 							<h2>Fichas de evaluación por ley de transparencia</h2>
 							<p>Son 33 fichas, una para cada ley de transparencia en México. Cada ficha contiene la calificación de los 196 criterios y el artículo que contiene el criterio evaluado o en caso de tener cero, el que estipula lo contrario. Ocasionalmente no se establece ningún artículo, esto sucede cuando no está contenido en ningún sitio de la ley.<br />
-							<a href="/data/fichas.zip">Fichas por ley de transparencia (zip 30M)</a></p>
+							<a href="/data/fichas.zip">Fichas por ley de transparencia (zip)</a></p>
 						</li>
-						<li>
+						<!--li>
 							<img src="/img/ad-fi.jpg" alt="thumb" />
 							<h2>Guía de interpretación</h2>
 							<p>Son 33 fichas, una para cada ley de transparencia en México. Cada ficha contiene la calificación de los 196 criterios y el artículo que contiene el criterio evaluado o en caso de tener cero, el que estipula lo contrario. Ocasionalmente no se establece ningún artículo, esto sucede cuando no está contenido en ningún sitio de la ley.<br />
 							<a href="">Guía de interpretación</a></p>
-							
-						</li>
+						</li-->
 						<li>
 							<img src="/img/ad-cu.jpg" alt="thumb" />
 							<h2>Cuadernillo de resultados</h2>
 							<p>En este documento se exponen los resultados del índice, así como su interpretación a la luz del debate actual. El análisis de los resultados presentados es una de muchas formas de hacerlo, para ello está disponible la base de datos de donde pueden obtener la información necesaria para hacer los cruces de información de interés de cada persona. En este cuadernillo se presentan los resultados por indicador, en donde buscamos, sobre todo, dar un panorama general del estado de<br />
-							<a href="/data/cuadernillos.pdf">Cuadernillo de resultados</a></p>
+							<a href="/data/cuadernillo.pdf">Cuadernillo de resultados</a></p>
 						</li>
 					</ul>
 				</article>
